33e5bf0487e3fb4aa2ce10ef98f1d4d22c1f5146
LLVM libc <llvm-libc@google.com>
2024-04-12 23:33:35 +0200

    Project import generated by Copybara.
    GitOrigin-RevId: 900be9013fdc3bab9fce906f8a71e59ecd8873b4
    Change-Id: I8609a042535f410747b4903f2de82e4de8ffebec

--- src/unistd/pipe.h
+++ src/unistd/pipe.h
@@ -1,4 +1,4 @@
-//===-- RandUtils.cpp -----------------------------------------------------===//
+//===-- Implementation header for pipe --------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "RandUtils.h"
-
-#include <cstdlib>
+#ifndef LLVM_LIBC_SRC_UNISTD_PIPE_H
+#define LLVM_LIBC_SRC_UNISTD_PIPE_H
 
 namespace LIBC_NAMESPACE {
-namespace testutils {
 
-int rand() { return std::rand(); }
+int pipe(int pipefd[2]);
 
-} // namespace testutils
 } // namespace LIBC_NAMESPACE
+
+#endif // LLVM_LIBC_SRC_UNISTD_PIPE_H
