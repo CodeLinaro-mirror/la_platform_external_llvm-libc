f59d65748d8d7d6d8fa3ef374a1d7fcd7637bc7f
LLVM libc <llvm-libc@google.com>
2025-04-25 09:55:21 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: ade502a8c46b8393e022b4eabcdd445af91a451c
    Change-Id: Iae8762a5e6be1a6157c4216f26eb2f4408236eff

--- include/llvm-libc-types/VISIT.h
+++ include/llvm-libc-types/VISIT.h
@@ -1,4 +1,4 @@
-//===-- Definition of type __lsearchcompare_t -----------------------------===//
+//===-- Definition of VISIT type ------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,9 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_TYPES___LSEARCHCOMPARE_T_H
-#define LLVM_LIBC_TYPES___LSEARCHCOMPARE_T_H
+#ifndef LLVM_LIBC_TYPES_VISIT_H
+#define LLVM_LIBC_TYPES_VISIT_H
 
-typedef int (*__lsearchcompare_t)(const void *, const void *);
+typedef enum { preorder, postorder, endorder, leaf } VISIT;
 
-#endif // LLVM_LIBC_TYPES___LSEARCHCOMPARE_T_H
+#endif // LLVM_LIBC_TYPES_VISIT_H
